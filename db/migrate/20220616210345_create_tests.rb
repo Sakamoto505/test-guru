41d1be0720587509e65713e904bd1a7773ac2996
new <new@MBP-Karina.home>
2022-06-17 01:49:43 +0300

    added started models and migrations

--- db/migrate/20220616210345_create_tests.rb
+++ db/migrate/20220616210345_create_tests.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :body, default: 0
-      t.references :categorie, foreign_key: true, null: false
+      t.references :category, foreign_key: true, null: false
 
       t.timestamps
     end
