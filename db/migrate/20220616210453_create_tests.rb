1d325ea9e6c32ad52dd18e807a315827074c2c80
new <new@MBP-Karina.home>
2022-06-17 15:35:03 +0300

    fixed after review

--- db/migrate/20220616210453_create_tests.rb
+++ db/migrate/20220616210453_create_tests.rb
@@ -6,6 +6,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
       t.string :title, null: false
       t.integer :body, default: 0
       t.references :category, foreign_key: true, null: false
+      t.references :author, foreign_key: { to_table: :users }
 
       t.timestamps
     end
