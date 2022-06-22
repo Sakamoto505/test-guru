5a4597a90042e87b665cc9c4130689ede8f92e36
new <new@MBP-Karina.home>
2022-06-22 12:56:34 +0300

    updated after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  def self.titles_by_category(category)
+  def self.titles_by_category(category_name)
     joins('join categories on tests.category_id = categories.id')
-      .where('categories.name = ?', category)
+      .where(categories: { name: category_name })
       .order(title: :DESC)
       .pluck(:title)
   end
