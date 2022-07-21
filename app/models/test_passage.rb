5d88e2664c351b7228aacdb13712c19587943b75
new <new@MBP-Karina.home>
2022-07-21 14:26:16 +0300

    Update correct_answer? in  hw8

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -41,7 +41,7 @@ class TestPassage < ApplicationRecord
   def correct_answer?(answer_ids)
     return false if answer_ids.nil?
 
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   private
