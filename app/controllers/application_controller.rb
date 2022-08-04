06ec65c8c9a70734d1c902c8d48461d65c96d2d7
new <new@MBP-Karina.home>
2022-08-04 20:12:51 +0300

    Added hw11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def default_url_options
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   protected
 
